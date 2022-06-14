3e14d79acb1026ff939c7df50ccf83cb5003e40b
AleksaPapovic <papovic.ra167.2018@uns.ac.rs>
2022-06-14 15:03:28 +0200

    feat: finished front

--- angular-kvzr/src/app/app-routing.module.ts
+++ angular-kvzr/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomepageComponent } from './pages/homepage/homepage.component';
+import { MalfunctionEvaluationComponent } from './pages/malfunction-evaluation/malfunction-evaluation.component';
 import { PurposeEvaluationComponent } from './pages/purpose-evaluation/purpose-evaluation.component';
 
 const routes: Routes = [
@@ -11,6 +12,10 @@ const routes: Routes = [
   {
     path: 'purposeEvaluation',
     component: PurposeEvaluationComponent,
+  },
+  {
+    path: 'malfunctionEvaluation',
+    component: MalfunctionEvaluationComponent,
   },
   { path: '**', redirectTo: '' },
 ];
